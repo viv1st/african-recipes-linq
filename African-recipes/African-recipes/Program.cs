0c09947d952149a1f8a9327f7fffeac6b3e4714e
Vivien Andriampenomalaza <stephensonvivien1@gmail.com>
2024-08-27 20:00:49 +0200

    modif fichiers

--- African-recipes/African-recipes/Program.cs
+++ African-recipes/African-recipes/Program.cs
@@ -1,78 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Xml.Linq;
-using African_recipes.collections;
-using Newtonsoft.Json;
-using FunctionsNamespace;
+﻿using AfricanRecipes;
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        // ---- DEBUT Réupération des données sous format XML et JSON ----
-        
+        string jsonFilePath = Functions.GetFileDirectory("/data_source.json");
+        string xmlFilePath = Functions.GetFileDirectory("/data_source.xml");
 
-        // Récup données à partir du JSON
+        string newJsonFilePath = "transformed_data_source.json";
+        string newXmlFilePath = "transformed_data_source.xml";
 
+        // Convertir JSON en XML
+        Functions.JsonFileToXmlFile(jsonFilePath, newXmlFilePath);
+        Console.WriteLine("Le fichier JSON a été converti en XML.");
 
-        // ---- FIN Réupération des données sous format XML et JSON ----
-
-        // Lire les données JSON et XML
-        var recettesData = Functions.ReadData();
-
-        // Exemple de recherche
-        var searchResults = Functions.SearchRecettes(recettesData, "Poulet");
-
-        // Exemple de tri
-        var sortedResults = Functions.SortRecettes(searchResults, "Nom");
-
-        // Exemple de condition de recherche
-        var filteredResults = Functions.FilterRecettes(sortedResults, r => r.Nom.Contains("Poulet"));
-
-        // Afficher les résultats
-        foreach (var recette in filteredResults)
-        {
-            Console.WriteLine($"Nom: {recette.Nom}, Origine: {recette.Origine}");
-        }
-
-        // Lire le fichier JSON
-        string jsonFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data.json");
-        string jsonData = File.ReadAllText(jsonFilePath);
-
-        // Désérialiser le JSON en objets C#
-        ListeRecettesData recettesDataInitial = JsonConvert.DeserializeObject<ListeRecettesData>(jsonData);
-
-        // Créer le document XML
-        XDocument xmlDocument = Functions.CreateXmlDocument(recettesDataInitial);
-
-        // Afficher le document XML
-        Console.WriteLine(xmlDocument);
-
-        // Définir le chemin du fichier XML
-        string projectRoot = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName;
-        string xmlDirectory = Path.Combine(projectRoot, "xml");
-        string xmlFilePath = Path.Combine(xmlDirectory, "Recettes.xml");
-
-        // Sauvegarder le document XML
-        Functions.SaveXmlDocument(xmlDocument, xmlDirectory, xmlFilePath);
-
-        // Lire le fichier XML et générer le fichier JSON
-        ListeRecettesData recettesData2 = Functions.ConvertXmlToJson(xmlFilePath);
-
-        // Sérialiser les objets C# en JSON
-        string json = JsonConvert.SerializeObject(recettesData2, Formatting.Indented);
-
-        // Afficher le JSON
-        Console.WriteLine(json);
-
-        // Définir le chemin du fichier JSON
-        string jsonDirectory = Path.Combine(projectRoot, "json");
-        string newjsonFilePath = Path.Combine(jsonDirectory, "Recettes.json");
-
-        // Sauvegarder le document JSON
-        Functions.SaveJsonDocument(json, jsonDirectory, newjsonFilePath);
+        // Convertir XML en JSON
+        Functions.XmlFileToJsonFile(xmlFilePath, newJsonFilePath);
+        Console.WriteLine("Le fichier XML a été converti en JSON.");
     }
-
 }
